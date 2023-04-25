8dc763c226e0fe25b486508838c15d80c6af05bb
anna-sahradyan <a.sahradyan72@gmail.com>
2023-04-25 17:21:22 +0400

    changes

--- index.js
+++ index.js
@@ -4,34 +4,36 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
-import path from "path";
+// import path from "path";
 //?Constants
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 dotenv.config();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client-test/build")))
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, "../", "client-test", "build", "index.html")
-        )
-    })
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../client-test/build")))
+//     app.get("*", (req, res) => {
+//         res.sendFile(
+//             path.resolve(__dirname, "../", "client-test", "build", "index.html")
+//         )
+//     })
+//
+// } else {
+//     app.get("/", (req, res) => {
+//         res.send("Home Page")
+//     })
+//
+// }
 
-} else {
-    app.get("/", (req, res) => {
-        res.send("Home Page")
-    })
-
-}
-
-
+app.get("/", (req, res) => {
+    res.send("Home Page")
+});
 //?Middleware
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
-app.use(`/user`,userRouter);
+app.use(`/user`, userRouter);
 //?Routes
 
 
